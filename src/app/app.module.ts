680b43baf8a95b604b85587f8db383a3afdd3786
Lucas Pélos <lucas.pelos@gmail.com>
2019-10-30 22:04:54 -0300

    core & share modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
